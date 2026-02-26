e189500cee96c46c17b653474ee51c93e59a5471
Umutcan Yavuz <158214796+umut392@users.noreply.github.com>
2026-02-26 13:52:09 +0300

    Create time_logic.py

---
+++ time_logic.py
@@ -0,0 +1,10 @@
+time=int(input("enter time"))
+dakika=int(time/60)
+
+dakika1=int(dakika%60)
+
+
+saat=int(dakika/60)
+saniye=time%60
+
+print(time," seconds is ",saat," hours, ",dakika1," minutes, and ",saniye," seconds.")
