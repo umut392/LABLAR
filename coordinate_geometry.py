985cffa0a3c1b13f320079373cbfb4973fc57157
Umutcan Yavuz <158214796+umut392@users.noreply.github.com>
2026-02-26 13:53:15 +0300

    Create coordinate_geometry.py

---
+++ coordinate_geometry.py
@@ -0,0 +1,13 @@
+x1=input("enter first value ")
+y1=input("enter second values ")
+x2=input("enter second values ")
+y2=input("enter second values ")
+
+x2eksix1inkaresi=(x2-x1)*(x2-x1)
+y2eksiy1inkaresi=(y2-y1)(y2-y1)
+kökiçi=x2eksix1inkaresi+y2eksiy1inkaresi
+sonuc=kökiçi**0.5
+print("distance ",sonuc)
+
+
+
